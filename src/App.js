b00a95bae887341273f94a653a13311a81d11c6d
Sean Stevens <sstevens88@gmail.com>
2021-08-11 14:02:59 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootswatch/dist/minty/bootstrap.min.css";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom"
+
+import Header from './Header';
+import Summaries from "./Summaries";
+import Activities from "./Activities";
+import Places from "./Places";
+import Navigation from "./Navigation";
+import { Container, Row } from "react-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Header />
+      <Container>
+        <Row>
+          <Navigation />
+        </Row>
+        <Row>
+          <Switch>
+            <Route path="/summaries">
+              <Summaries />
+            </Route>
+            <Route path="/activities">
+              <Activities />
+            </Route>
+            <Route path="/places">
+              <Places />
+            </Route>
+          </Switch>
+        </Row>
+      </Container>
+    </Router>
   );
 }
 
